7713fbd6a69f91de3893d74b6a19bd6061cb90eb
Mycicle <mjm.digregorio@gmail.com>
2020-11-17 08:36:45 -0500

    boost: added multiprecision numbers from the boost library so we can support uint256_t operations
    Signed-off-by: Mycicle <mjm.digregorio@gmail.com>

--- src/fermat.cc
+++ src/fermat.cc
@@ -1,14 +1,17 @@
 #pragma once
 #include <cmath>
 #include <random>
+#include "boost/multiprecision/cpp_int.hpp"
 #include "powermod.cc"
 
 std::default_random_engine gen(0);
 
-bool fermat(long long int p, int k) {
-    std::uniform_int_distribution<long long int> distribution((long long int)(p-1)/(10), p-1);
+typedef boost::multiprecision::uint256_t uint256_t;
+
+bool fermat(uint256_t p, int k) {
+    std::uniform_int_distribution<uint256_t> distribution((uint256_t)(p-1)/(10), p-1);
     for (int i = 0; i < k; i++) {
-        long long int a = distribution(gen);
+        uint256_t a = distribution(gen);
         if (powermod(a, p-1, p) != 1) {
             return false;
         }
--- src/powermod.cc
+++ src/powermod.cc
@@ -1,14 +1,18 @@
 #pragma once
 #include <cmath>
+#include "boost/multiprecision/cpp_int.hpp"
 
-long double powermod(long long int x, long long int n, long long int m) {
+typedef boost::multiprecision::uint256_t uint256_t;
+
+uint256_t powermod(uint256_t x, uint256_t n, uint256_t m) {
+    namespace mp = boost::multiprecision;
     // for x^n mod m
-    long long int prod = 1;
+    uint256_t prod = 1;
     while (n > 0) {
         if (n % 2 != 0) {
             prod = (prod*x) % m;
         }
-        x = (long long int)std::pow(x, 2) % m;
+        x = (uint256_t) mp::pow(mp::cpp_int(x), 2) % m;
         n = n/2;
     }
     return prod;
